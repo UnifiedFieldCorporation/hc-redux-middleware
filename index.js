7ab5653b1e999fc6df6746795cfe3e92b0fafbcb
Connor Turland <connorturland@gmail.com>
2019-03-27 12:07:03 -0400

    updates

--- index.js
+++ index.js
@@ -1,69 +1,55 @@
-'use strict';
+var axios = require('axios')
 
-Object.defineProperty(exports, "__esModule", {
-  value: true
-});
-exports.requestSendingMiddleware = requestSendingMiddleware;
-exports.send = send;
-exports.hcMiddleware = hcMiddleware;
-
-var _axios = require('axios');
-
-var _axios2 = _interopRequireDefault(_axios);
-
-function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
-
-function requestSendingMiddleware(store) {
-  return function (next) {
-    return function (action) {
-      var payload = action.payload,
-          meta = action.meta,
-          type = action.type;
-      // if its a network request
-      // fire an action that indicates the request has
-      // been sent
-
-      if (payload && payload.then) {
-        var requestSendingAction = {
-          payload: null,
-          type: type + 'Sent',
-          meta: meta
-        };
-        store.dispatch(requestSendingAction);
+exports.requestSendingMiddleware = function requestSendingMiddleware (store) {
+  return next => action => {
+    const { payload, meta, type } = action
+    // if its a network request
+    // fire an action that indicates the request has
+    // been sent
+    if (payload && payload.then) {
+      const requestSendingAction = {
+        payload: null,
+        type: type + 'Sent',
+        meta
       }
-      return next(action);
-    };
-  };
-}
-
-function send(namespace, fnName, data, baseUrl) {
-  return _axios2.default.post(baseUrl + '/fn/' + namespace + '/' + fnName, data).then(function (res) {
-    if (typeof res.data === "string" && res.data.indexOf("Error") > -1) {
-      return Promise.reject(new Error(res.data));
+      store.dispatch(requestSendingAction)
     }
-    return Promise.resolve(res.data);
-  }).catch(function (err) {
-    console.log(err);
-  });
+    return next(action)
+  }
 }
 
-function hcMiddleware(store) {
-  return function (next) {
-    return function (action) {
-      var type = action.type,
-          meta = action.meta;
+exports.send = function send (namespace, fnName, data, baseUrl) {
+    // HACK append a localStorage saved password onto every request as the security model for now
+    const pw = localStorage.getItem('touchpoints_pw')
+    return axios.post(`${baseUrl}/fn/${namespace}/${fnName}?pw=${pw}`, data)
+        .then(res => {
+            if (typeof res.data === "string" && res.data.indexOf("Error") > -1) {
+                return Promise.reject(new Error(res.data))
+            }
+            return Promise.resolve(res.data)
+        })
+        .catch(err => {
+            console.log(err)
+        })
+}
 
-      if (!(meta && meta.isHc)) return next(action);
-      // the rest will be handled by redux-promises
-      var baseUrl = meta.baseUrl || '';
-      var sendRequest = send(meta.namespace, type, meta.data, baseUrl);
-      sendRequest = meta.then ? sendRequest.then(meta.then) : sendRequest;
-      var newAction = Object.assign({}, action, {
-        payload: sendRequest
-      }, {
-        meta: Object.assign({}, meta, { isHc: false })
-      });
-      return next(newAction);
-    };
-  };
+exports.hcMiddleware = function hcMiddleware (store) {
+    return next => action => {
+        const { type, meta } = action
+        if (!(meta && meta.isHc)) return next(action)
+        // the rest will be handled by redux-promises
+        const baseUrl = meta.baseUrl || ''
+        let sendRequest = send(meta.namespace, type, meta.data, baseUrl)
+        sendRequest = meta.then ? sendRequest.then(meta.then) : sendRequest
+        const newAction = Object.assign({},
+          action,
+          {
+            payload: sendRequest
+          },
+          {
+            meta: Object.assign({}, meta, { isHc: false })
+          }
+        )
+        return next(newAction)
+    }
 }
